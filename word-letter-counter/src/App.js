0d6f0b6377070bcbe6f6d29dffe5dccdda6ed9aa
dkg2395 <dkg.2395.com>
2024-04-06 20:32:21 +0530

    word -letter count

--- word-letter-counter/src/App.js
+++ word-letter-counter/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import WordLetterCounter from "./WordLetterCounter"; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Welcome to word count app</h1>
+    <WordLetterCounter /> 
     </div>
   );
 }
